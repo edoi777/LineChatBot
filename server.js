57bd13eff9ae5347ab67d43315e89e715504c8e2
Join Jirapa <JoiN_WoO@hotmail.com>
2018-07-12 11:05:41 +0700

    edit server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-    channelAccessToken: '94RSe503vUxRDUgFDsTiS8VhWLAw0VhItUPhNu0howCKhOLMTdj2KzM+cMPZzIoyFPFishj7/aIMFAges23hAsZI6YRIKEEkTKa/ztVSECn/EymB4YyS7tD5r7YCMMKz2kDAXIezA0p/vMIw+jhn4QdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '985467c5007c52ffc04b485e6bccb3cb'
+    channelAccessToken: 'zaKwcTFahinrF8TlU7lu0/wUaNiFwFq32yKM/Wllt4gczGLU/5qLWF5CJLSfbadDFPFishj7/aIMFAges23hAsZI6YRIKEEkTKa/ztVSECm/ggXZi1Btrk2lOwxHxWoXlHBuAfR49ofREcAiQIBheAdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'aa4e48ba5b1b8eb471d0d9ebca1b46a2'
 };
 
 const app = express();
