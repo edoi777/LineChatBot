005183ec30330f62ff86d0e3d7f9c3b3fe7c06a4
Join Jirapa <JoiN_WoO@hotmail.com>
2018-07-12 13:27:01 +0700

    edit server.js

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const config = {
 };
 
 
-const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
+}
+
+app.listen(3000);
